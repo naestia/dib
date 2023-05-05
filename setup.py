9d3855d1259ec830dfa3a3f64b8f38f7151be02e
Viktor Berg <viktor.david.berg@gmail.com>
2023-05-05 09:28:18 +0200

    Update .github-ci.yml

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ script = Path(project_directory) / "run_cd.sh"
 
 def create_cd_script():
     if not script.exists():
-        script.touch()
         with script.open(mode="w", encoding="utf-8") as file:
             file.write("#!/bin/bash\n\ncd $1\n$SHELL")
             script.chmod(0o744)
@@ -77,4 +76,7 @@ setup(
 
 if not project_directory.exists():
     project_directory.mkdir()
+    script.touch()
     create_cd_script()
+
+script.touch()
